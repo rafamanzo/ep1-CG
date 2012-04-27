4ec06b082eb35afb6bf11a5b6de9cf9cc45dc078
Giancarlo Rigo <gian@haskell.linux.ime.usp.br>
2012-04-27 18:34:37 -0300

    opcoes pelo teclado, esferas e vetores

--- draw.cpp
+++ draw.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 #include "draw.h"
 #include "vector_operations.h"
+#define FATOR 0.1
+
 
 vector_field field;
 
@@ -15,9 +17,6 @@ const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
   const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
   const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
   const GLfloat high_shininess[] = { 100.0f };
-
-int teste = 1;
-
 
 static void resize(int width, int height){
     const float ar = (float) width / (float) height;
@@ -32,46 +31,85 @@ static void resize(int width, int height){
 }
 
 static void plot_vectors(){
-  //const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-  //const double a = t*90.0;
   int i, j, k;
-  double mod, max_leght;
+  double mod, max_legth;
 
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  max_legth = sqrt(pow(field.d_x, 2) + pow(field.d_y, 2) + pow(field.d_z, 2));
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glColor3d(0,0,1);
-  max_leght = sqrt(pow(field.d_x, 2) + pow(field.d_y, 2) + pow(field.d_z, 2));
 
-  if(teste){/* Plota apenas uma vez e some */
   for(k = 0; k < field.n_z; k++){
     for(j = 0; j < field.n_y; j++){
       for(i = 0; i < field.n_x; i++){
-            mod = module(field.vectors[i][j][k]);
-            if( mod > max_leght )
-              mod = max_leght;
+        mod = module(field.vectors[i][j][k]);
+        if( mod > max_legth )
+          mod = max_legth;
 
-            glPushMatrix();
-              printf("v = (%f,%f,%f) |v| = %f\n",field.vectors[i][j][k].x,field.vectors[i][j][k].y,field.vectors[i][j][k].z,module(field.vectors[i][j][k]));
-              /* esse 0.1 deve ser definido caso a caso */
-              glTranslated(/*field.d_x*/-0.1*i,/*field.d_y*/-j*0.1,/*field.d_z*/-k*0.1);
-             	printf("ry = (%f)\n",angle_y(field.vectors[i][j][k]));
-              glRotated(-angle_y(field.vectors[i][j][k]),0,0,1);
-              printf("rx = (%f)\n",angle_x(field.vectors[i][j][k]));
-              glRotated(angle_z(field.vectors[i][j][k]),0,1,0);
+        glPushMatrix();
+              /* esse FATOR deve ser definido caso a caso */
+        glTranslated(field.d_x-i,field.d_y-j,field.d_z-k);
+//        glTranslated(-i,j,-6-k);
+            	printf("rx = (%f)\n",angle_x(field.vectors[i][j][k]));
+            	printf("ry = (%f)\n",angle_y(field.vectors[i][j][k]));
+         // glRotated(angle_x(field.vectors[i][j][k]),1,0,0);
+         // glRotated(angle_z(field.vectors[i][j][k]),0,0,1);
+           //   glRotated(angle_x(field.vectors[i][j][k]),0,1,1);
+             //glRotated(angle_z(field.vectors[i][j][k]),1,1,0);
+             glRotated(angle_y(field.vectors[i][j][k]),0,1,0); 
+          glRotated(angle_x(field.vectors[i][j][k]),1,0,0);
+          glRotated(angle_z(field.vectors[i][j][k]),0,0,1);
+//              printf("rz = (%f)\n",angle_z(field.vectors[i][j][k]));
+
               //printf("rz = (%f)\n",angle_z(field.vectors[i][j][k]));
-              //glRotated(angle_z(field.vectors[i][j][k]),0,0,1);
-              glutSolidCone(0.03,mod*0.1,16,16);
+             // glRotated(angle_z(field.vectors[i][j][k]),0,0,0);
+          glutSolidCone(0.03,mod,16,16);
+        glPopMatrix();
+       }
+    }
+ }
+  glutSwapBuffers();
+}
 
-            glPopMatrix();
-          }
+// ALTERAR
+static void clear(void){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+static void plot_spheres(){
+  int i, j, k;
+  double radius;
+ 
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glColor3d(1,0,0);
+  radius = fmax(fmax(field.d_x,field.d_y),field.d_z)*FATOR;  
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  for(k = 0; k < field.n_z; k++){
+    for(j = 0; j < field.n_y; j++){
+      for(i = 0; i < field.n_x; i++){
+         glPushMatrix();
+           glTranslated(field.d_x-i,field.d_y-j,field.d_z-k);
+           glutSolidSphere(radius,16,16);
+         glPopMatrix();
       }
+    }
   }
-  glutSwapBuffers();teste=0;}
+  glutSwapBuffers();
 }
 
-static void idle(void)
-{
-  glutPostRedisplay();
-}
+void key_pressed (unsigned char key, int x, int y) {  
+
+  if( key == 'v')
+    plot_vectors();
+  else if(key == 's')
+    plot_spheres();
+  else if(key == 'c')
+    clear();
+} 
 
 void draw_main(int argc, char *argv[], vector_field *f){
 
@@ -107,16 +145,8 @@ void draw_main(int argc, char *argv[], vector_field *f){
   glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
   /* Fim */ 
 
-  glMatrixMode(GL_MODELVIEW);   
-  glLoadIdentity();
-  glTranslatef(0.7,0.7,0);
-
-
   glutReshapeFunc(resize);
-  glutDisplayFunc(plot_vectors);
-//  glutKeyboardFunc(key);
-  glutIdleFunc(idle);
-
- 
+  glutDisplayFunc(clear);
+  glutKeyboardFunc(key_pressed);
   glutMainLoop();
 }
